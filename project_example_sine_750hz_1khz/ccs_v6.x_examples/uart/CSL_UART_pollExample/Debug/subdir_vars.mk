2d8bc0e941a12ced51334ae18fbb30589391b11e
caonguyenheo <caonguyen1907@gmail.com>
2018-07-25 09:21:21 +0700

    new

---
+++ project_example_sine_750hz_1khz/ccs_v6.x_examples/uart/CSL_UART_pollExample/Debug/subdir_vars.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../VC5505_UART.cmd 
+
+C_SRCS += \
+../aic3204_config.c \
+../aic3204_loop_mic_in.c \
+../i2s_Intr.c \
+../main.c 
+
+C_DEPS += \
+./aic3204_config.d \
+./aic3204_loop_mic_in.d \
+./i2s_Intr.d \
+./main.d 
+
+OBJS += \
+./aic3204_config.obj \
+./aic3204_loop_mic_in.obj \
+./i2s_Intr.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"aic3204_config.obj" \
+"aic3204_loop_mic_in.obj" \
+"i2s_Intr.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"aic3204_config.d" \
+"aic3204_loop_mic_in.d" \
+"i2s_Intr.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../aic3204_config.c" \
+"../aic3204_loop_mic_in.c" \
+"../i2s_Intr.c" \
+"../main.c" 
+
+
